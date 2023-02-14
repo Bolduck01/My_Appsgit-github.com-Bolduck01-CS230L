14bd301ef6395115e28586a1991a08bd2ffb707e
Brendan Bolduc <bab00038@mix.wvu.edu>
2023-02-14 17:13:55 -0500

    Lab1 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Navbar1Component } from './header/navbar1/navbar1.component';
 import { Navbar2Component } from './footer/navbar2/navbar2.component';
+import { Carousel1Component } from './carousel1/carousel1.component';
+import { ProductCard1Component } from './product-card1/product-card1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Navbar1Component,
-    Navbar2Component
+    Navbar2Component,
+    Carousel1Component,
+    ProductCard1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-card1/product-card1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card1',
+  templateUrl: './product-card1.component.html',
+  styleUrls: ['./product-card1.component.css']
+})
+export class ProductCard1Component {
+
+}
