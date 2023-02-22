b0f35199dd98d2065f69cd48fd792cd53408b117
Brendan Bolduc <bab00038@mix.wvu.edu>
2023-02-21 23:06:09 -0500

    Lab3

---
+++ src/app/product-card/mock_lists.ts
@@ -0,0 +1,24 @@
+export const mock_list = [ 
+    {
+        img: "https://hbomax-images.warnermediacdn.com/2021-04/artboard_1_copy_7_0.png?host=wme-hbomax-drupal-prod.s3.amazonaws.com&w=1024",
+        description: "ad background for pc2",
+        imgDescription: "ad"
+    },
+
+    {
+        img: "https://www.series-80.net/wp-content/uploads/2021/05/1619864325_Le-createur-Entourage-pense-que-HBO-Max-cache-une.jpg" ,
+        description: "corousel1",
+        imgDescription: "Entourage",
+    },
+
+    {
+        img: "https://mbt.mv/wp-content/uploads/2022/08/house_of_the_dragon-1536x864.jpeg",
+        description: "corousel1",
+        imgDescription: "house dragon",
+    },
+    {
+        img: "https://i.imgur.com/puzX37U.jpeg",
+        description: "carousel1",
+        imgDescription: "Rick n Morty",
+    }
+ ]
